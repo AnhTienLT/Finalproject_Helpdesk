54a89ad30240b3de97b5a935a2ac40ac51a63455
AnhTienLT <hoanganhtien211@gmail.com>
2026-08-06 19:57:49 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return redirect('/login');
